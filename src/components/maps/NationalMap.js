5476bb6cab36285815e7f9ad77caa9c2fb0f03cc
Alex <am3081@gmail.com>
2016-04-11 12:32:53 -0400

    responsive national map

--- src/components/maps/NationalMap.js
+++ src/components/maps/NationalMap.js
@@ -2,99 +2,90 @@
 import React from 'react'
 import d3 from 'd3'
 import topojson from 'topojson'
+import classes from './NationalMap.scss'
 
 var backColor = "#DCDCDC",
     msaColor = "#7EC0EE";
 
 export class NationalMap extends React.Component<void, Props, void> {
-  
+  constructor () {
+    super()
+    this.state = {
+      statesGeo: null, 
+      metrosGeo: null,
+      resizing: false
+    }
+    this._initGraph = this._initGraph.bind(this)
+    this._drawGraph = this._drawGraph.bind(this)
+  }
+
   componentDidMount (){
-    this.renderGraph()
+    this._initGraph()
   }
   
-  renderGraph () {
-      let width = 960,
-          height = 600;
+  _drawGraph () {
+    let statesGeo = this.state.statesGeo
+    let metrosGeo = this.state.metrosGeo
+    let width = document.getElementById("mapDiv").offsetWidth
+    let height = width  * 0.6
 
-      let projection = d3.geo.albersUsa()
-          .scale(1000)
-          .translate([width / 2.5, height / 2.5]);
+    var projection = d3.geo.albersUsa();
 
-      let path = d3.geo.path()
-          .projection(projection);
+    var path = d3.geo.path()
+      .projection(projection);
 
-      let svg = d3.select("#mapDiv").append("svg")
-          .attr("width", width)
-          .attr("height", height);
+    projection
+        .scale(1)
+        .translate([0, 0]);
 
-      d3.json('/us.json', function(err,us){
-        console.log(Object.keys(us.objects))
-        svg.append("path")
-          .datum(topojson.feature(us,us["objects"]["states.geo"]))
-          .style("fill",backColor)
-          .attr("d", path);
+    var b = path.bounds(statesGeo),
+        s = .95 / Math.max((b[1][0] - b[0][0]) / width, (b[1][1] - b[0][1]) / height),
+        t = [(width - s * (b[1][0] + b[0][0])) / 2, (height - s * (b[1][1] + b[0][1])) / 2];
 
-        svg.insert("path", ".graticule")
-              .datum(topojson.mesh(us,us["objects"]["states.geo"], function(a, b) { return a !== b; }))
-              .style("fill", "none")
-              .style("stroke","#fff")
-              .attr("d", path);
+    projection
+        .scale(s)
+        .translate(t);
 
-        svg.selectAll("path", ".msa")
-              .data(topojson.feature(us,us["objects"]["fixMsa.geo"]).features)
-              .enter().append('path')
-              .attr("class","msa")
-              .attr("id",function(d){return "msa"+d.properties.id;})
-              .style("fill", msaColor)
-              .style("stroke","#fff")
-              .attr("d", path)
-              .on('click',click);
+    let svg = d3.select("#mapDiv svg")
+    .attr('viewBox','0 0 ' + width + ' ' + height)
+    
+    svg.selectAll(".state")
+      .data(statesGeo.features)
+      .enter().append('path')
+      .attr('class','state')
+      .style("fill",backColor)
+      .attr("d", path);
+
+
+    svg.selectAll(".msa")
+      .data(metrosGeo.features)
+      .enter().append('path')
+      .attr("class","msa")
+      .attr("id",function(d){return "msa"+d.properties.id;})
+      .style("fill", msaColor)
+      .style("stroke","#fff")
+      .attr("d", path)
+      .on('click',this.props.click || null);
+  }
+
+  _initGraph () {
+    d3.json('/us.json',(err,us) => {
+      this.setState({
+        statesGeo:topojson.feature(us,us["objects"]["states.geo"]),
+        metrosGeo: topojson.feature(us,us["objects"]["fixMsa.geo"])
       })
+      this._drawGraph()
+    })
+  }
 
-      function click(d){
-        this.props.click(d)
-      }
+  render () {
 
-    }
-    render () {
-        
-        const legendStyle = {
-          float:'left',
-          width:'18%',
-          height:'300px',
-          boxShadow:'2px 2px' + backColor,
-          paddingLeft:'10px',
-          paddingTop:'2px',
-          paddingRight:'10px',
-          paddingBottom:'2px',
-          marginLeft:'40px',
-          marginRight:'200px',
-          marginTop:'15px',
-          background:backColor,
-          color:"#black"
-        }
-
-        const headerStyle = {
-          textAlign:'center',
-          marginBottom:'0px',
-          borderBottom:'2px solid black'
-        }
-
-        const contentStyle={
-          marginTop:'5px'
-        }
-
-        return (
-            <div>
-                <div id="msaLegend" style={legendStyle}>
-                  <div id="msaLegendHeader" style={headerStyle}><h4>Metropolitan Statistical Area</h4></div>
-                  <div id="msaLegendContent" style={contentStyle}></div>
-                </div>
-                <div id="mapDiv"></div>
-            </div>
-                
-        )
-    }
+    return (
+      <div id="mapDiv" className={classes['svg-container']}>
+        <svg className={classes['.svg-content-responsive']} preserveAspectRatio='xMinYMin meet'/>
+      </div>
+    )
+  }
 }
 
 export default  NationalMap
