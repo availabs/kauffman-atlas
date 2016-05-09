d79d000e98d1dc0ce56c35c4958c56daa708b675
RyanDubowsky <r.k.dubowsky@gmail.com>
2016-05-09 15:04:08 -0400

    Made combined view/graph with three composite metrics. Need to create combined composite still

---
+++ src/components/graphs/CombinedGraph.js
@@ -0,0 +1,74 @@
+"use strict"
+import React from 'react'
+import { connect } from 'react-redux'
+import { loadDensityData,loadDensityComposite } from 'redux/modules/densityData'
+import { loadFluidityData,loadFluidityComposite } from 'redux/modules/fluidityData'
+import { loadDiversityData,loadDiversityComposite } from 'redux/modules/diversityData'
+import classes from '../../components/maps/NationalMap.scss'
+import LineGraph from '../../components/graphs/LineGraph.js'
+
+export class CombinedGraph extends React.Component<void, Props, void> {
+  constructor () {
+    super()
+    this.state = {
+      data:null,
+      loaded:false,
+      plot:"rank",
+      dataType:"raw"
+    }
+    this._initGraph = this._initGraph.bind(this)
+  }
+
+  componentWillMount () {
+    this._initGraph();
+  }
+ 
+  _initGraph () {
+    var dataset = this.props.selectedMetric.split("composite")[0];
+    console.log(dataset)
+
+    if(!this.props[(dataset + "loaded")]){
+      return this.props[(['load' + dataset + "Data"])]()          
+    }       
+    if(!this.props[(this.props.selectedMetric)]){
+      console.log('dont have metric')
+      return this.props[(['get' + this.props.selectedMetric])]()
+    }
+  }
+
+  render () {
+    this._initGraph();
+    console.log(this.props.selectedMetric)
+    if(this.props[(this.props.selectedMetric)]){
+      console.log("combined render making line chart")
+     return (
+        <LineGraph data={this.props[this.props.selectedMetric]} plot={this.props.plot} dataType={this.props.dataType} title={this.props.selectedMetric} graph={this.props.selectedMetric}/>
+      )         
+    }
+    else{
+      console.log("rener mia",this.props);
+      return (
+        <div></div>
+      )      
+    }
+
+  }
+}
+
+const mapStateToProps = (state) => ({
+  densitycomposite:state.densityData.compositeData,
+  densityloaded:state.densityData.loaded,
+  fluiditycomposite:state.fluidityData.compositeData,
+  fluidityloaded:state.fluidityData.fluLoaded,
+  diversityloaded : state.diversityData.diversityLoaded,
+  diversitycomposite : state.diversityData.diversitycomposite
+})
+
+export default connect((mapStateToProps), {
+  loaddensityData: () => loadDensityData(),
+  getdensitycomposite: () => loadDensityComposite(),
+  loadfluidityData: () => loadFluidityData(),
+  getfluiditycomposite: () => loadFluidityComposite(),
+  loaddiversityData: () => loadDiversityData (),
+  getdiversitycomposite: () => loadDiversityComposite ()
+})(CombinedGraph)
