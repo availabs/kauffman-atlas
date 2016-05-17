6b46a5e73faeda97ce367fea079557978fbd3bd9
RyanDubowsky <r.k.dubowsky@gmail.com>
2016-05-17 11:51:29 -0400

    Added workaround for issue caused in previous commit -- onclick inheritence resulted in the top 5 lists not linking, this simply adds the ID to all children divs

--- src/views/HomeView/HomeView.js
+++ src/views/HomeView/HomeView.js
@@ -72,13 +72,16 @@ export class HomeView extends React.Component<void, Props, void> {
     var topFiveCombined;
 
     let msaClick = (d) =>{
-      console.log(d);
+      console.log(d.id);
+
+
       if(d.id){
        this.props.history.push('/metro/'+d.id);       
       }
       else{
         this.props.history.push('/metro/'+d.target.id);  
-      }
+      }        
+    
 
     }
 
@@ -92,7 +95,7 @@ export class HomeView extends React.Component<void, Props, void> {
       topFiveDensity = Object.keys(topFiveDensity).map(rank => {
         var roundFormat = d3.format(".2f")
         return(
-              <div onClick={msaClick} id={topFiveDensity[rank].id} className={classes["msa"]}><div className={classes["name"]}>{rank + ". " + topFiveDensity[rank]["name"]}</div> <div className={classes["score"]}>{roundFormat(topFiveDensity[rank]["score"])}</div></div>
+              <div onClick={msaClick}  className={classes["msa"]}><div id={topFiveDensity[rank].id} className={classes["name"]}>{rank + ". " + topFiveDensity[rank]["name"]}</div> <div id={topFiveDensity[rank].id} className={classes["score"]}>{roundFormat(topFiveDensity[rank]["score"])}</div></div>
         )
       })
     }
@@ -110,7 +113,7 @@ export class HomeView extends React.Component<void, Props, void> {
       topFiveFluidity = Object.keys(topFiveFluidity).map(rank => {
         var roundFormat = d3.format(".2f")
         return(
-              <div onClick={msaClick} id={topFiveFluidity[rank].id} className={classes["msa"]}><div className={classes["name"]}>{rank + ". " + topFiveFluidity[rank]["name"]}</div> <div className={classes["score"]}>{roundFormat(topFiveFluidity[rank]["score"])}</div></div>
+              <div onClick={msaClick} id={topFiveFluidity[rank].id} className={classes["msa"]}><div id={topFiveFluidity[rank].id} className={classes["name"]}>{rank + ". " + topFiveFluidity[rank]["name"]}</div> <div id={topFiveFluidity[rank].id} className={classes["score"]}>{roundFormat(topFiveFluidity[rank]["score"])}</div></div>
         )
       })
     }
@@ -128,7 +131,7 @@ export class HomeView extends React.Component<void, Props, void> {
       topFiveDiversity = Object.keys(topFiveDiversity).map(rank => {
         var roundFormat = d3.format(".2f")
         return(
-              <div onClick={msaClick} id={topFiveDiversity[rank].id} className={classes["msa"]}><div className={classes["name"]}>{rank + ". " + topFiveDiversity[rank]["name"]} </div><div className={classes["score"]}>{roundFormat(topFiveDiversity[rank]["score"])}</div></div>
+              <div onClick={msaClick} id={topFiveDiversity[rank].id} className={classes["msa"]}><div id={topFiveDiversity[rank].id} className={classes["name"]}>{rank + ". " + topFiveDiversity[rank]["name"]} </div><div id={topFiveDiversity[rank].id} className={classes["score"]}>{roundFormat(topFiveDiversity[rank]["score"])}</div></div>
         )
       })
     }
@@ -146,7 +149,7 @@ export class HomeView extends React.Component<void, Props, void> {
       topFiveCombined = Object.keys(topFiveCombined).map(rank => {
         var roundFormat = d3.format(".2f")
         return(
-              <div onClick={msaClick} id={topFiveCombined[rank].id} className={classes["msa"]}> <div className={classes["name"]}>{rank + ". " + topFiveCombined[rank]["name"]}</div> <div className={classes["score"]}>{roundFormat(topFiveCombined[rank]["score"])}</div></div>
+              <div onClick={msaClick} id={topFiveCombined[rank].id} className={classes["msa"]}> <div id={topFiveCombined[rank].id} className={classes["name"]}>{rank + ". " + topFiveCombined[rank]["name"]}</div> <div id={topFiveCombined[rank].id} className={classes["score"]}>{roundFormat(topFiveCombined[rank]["score"])}</div></div>
         )
       })
     }
