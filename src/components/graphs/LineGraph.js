a6ac1ca07d5bb46cae4151ae5302992e47bdfe0f
RyanDubowsky <r.k.dubowsky@gmail.com>
2016-05-17 11:08:44 -0400

    Fixed x axis label for combined composite line graph

--- src/components/graphs/LineGraph.js
+++ src/components/graphs/LineGraph.js
@@ -170,16 +170,15 @@ export class LineGraph extends React.Component<void, Props, void> {
 
 
       if(scope.props.plot != 'rank'){
-        if(scope.props.graph != "combinedcomposite"){
-          xAxis.ticks(x.domain()[1]-x.domain()[0])             
-        }
-    
         if(scope.props.dataType != "raw" && scope.props.graph != "newValues" && (scope.props.graph.substr(-9)) != "composite" && scope.props.graph != "inc"){
             yAxis.tickFormat(axisPercFormat);
         }
-        if(scope.props.dataType != "raw" && scope.props.graph == "inc"){
+        else if(scope.props.dataType != "raw" && scope.props.graph == "inc"){
             yAxis.tickFormat(percFormat);         
-        }          
+        }    
+        else{
+          xAxis.ticks(x.domain()[1]-x.domain()[0])           
+        }      
       }
 
 
