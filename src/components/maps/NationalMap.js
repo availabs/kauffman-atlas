128bbbf5ba98d39b76c3667fba74380f989a672b
RyanDubowsky <r.k.dubowsky@gmail.com>
2016-05-19 12:26:27 -0400

    Fixed bug with mouseout on map where it would always return to the first color rendered

--- src/components/maps/NationalMap.js
+++ src/components/maps/NationalMap.js
@@ -20,8 +20,8 @@ export class NationalMap extends React.Component<void, Props, void> {
     this._initGraph = this._initGraph.bind(this)
     this._drawGraph = this._drawGraph.bind(this)
     this._drawMetros = this._drawMetros.bind(this)
-    this._colorMetros = this._colorMetros.bind(this)
     this._msaClick = this._msaClick.bind(this)
+    this._mouseout = this._mouseout.bind(this)
   }
 
   componentDidMount (){
@@ -45,8 +45,6 @@ export class NationalMap extends React.Component<void, Props, void> {
   }
 
   _colorMetros(props){
-    let metrosGeo = Object.assign({},props.metrosGeo);
-
     d3.selectAll("."+classes['msa'])
       .style("fill",function(d){   
         var color = "chartreuse"  
@@ -96,22 +94,13 @@ export class NationalMap extends React.Component<void, Props, void> {
     let svg = d3.select("#mapDiv svg")
       .attr('viewBox','0 0 ' + width + ' ' + height)
 
-    //Focus is the hover popup text
-    var focus = svg.append("g")
-          .attr("transform", "translate(-100,-100)")
-          .attr("class", "focus");
-
-    focus.append("text")
-      .attr("y", 10)
-      .style("font-size",".75em");
-    
+    //Remove all of the old metros
     d3.selectAll("."+classes['msa']).remove()
 
     svg.selectAll(".msa")
       .data(metrosGeo.features)
       .enter().append('path')
       .attr("class",classes['msa'])
-      .attr("id",function(d){d.shape =this; return "msa"+d.id;})
       .attr("d", path)
       .style("fill",function(d){   
         var color = "chartreuse"  
@@ -122,64 +111,15 @@ export class NationalMap extends React.Component<void, Props, void> {
         })
         return color;
       })
-      .on("mouseover", mouseover)
-      .on("mouseout", mouseout)
+      .attr("id",function(d){d.shape =this; return "msa"+d.id;})
+      .on("mouseover", this._mouseover)
+      .on("mouseout", this._mouseout)
       .on('click',this._msaClick);
-
-    function shadeRGBColor(color, percent) {
-        var f=color.split(","),t=percent<0?0:255,p=percent<0?percent*-1:percent,R=parseInt(f[0].slice(4)),G=parseInt(f[1]),B=parseInt(f[2]);
-        return "rgb("+(Math.round((t-R)*p)+R)+","+(Math.round((t-G)*p)+G)+","+(Math.round((t-B)*p)+B)+")";
-    }
-
-    function mouseover(d) {
-
-      var oldColor = d3.select(d.shape).style("fill")
-      d3.select(d.shape).style("fill",shadeRGBColor(oldColor,-.2))
-
-      var popText = "",
-          name;
-
-      name = d.properties.NAME;
- 
-      popText += name 
-
-      focus.attr("transform", "translate(50,0)");
-      focus.select("text").text(popText);
-
-    }
-
-    function mouseout(d) {                              
-      focus.attr("transform", "translate(-100,-100)");
-
-      var oldColor = d3.select(d.shape).style("fill")
-
-      d3.select(d.shape).style("fill",function(d){   
-        var color = "chartreuse"  
-        props[(props.activeComponent+"composite")].forEach(metroArea => {
-          if(metroArea.key == d.id){
-            color = metroArea.color;
-          }
-        })
-        return color;
-      })
-    }
   }
 
   _drawGraph (props) {
     let statesGeo = props.statesGeo
     let metrosGeo = Object.assign({},props.metrosGeo);
-
-    metrosGeo.features = metrosGeo.features.filter(d => {
-      var inBucket = false;
-      props.metros.forEach(msaId => {
-        if(d.id == msaId){
-          inBucket = true;
-        } 
-      })
-      return inBucket;
-    })        
-    console.log(metrosGeo)
-
 
     let width = document.getElementById("mapDiv").offsetWidth
     let height = width  * 0.6
@@ -219,12 +159,10 @@ export class NationalMap extends React.Component<void, Props, void> {
       .attr('class',classes['state'])
       .attr("d", path);
 
-
     svg.selectAll(".msa")
       .data(metrosGeo.features)
       .enter().append('path')
       .attr("class",classes['msa'])
-      .attr("id",function(d){d.shape =this; return "msa"+d.id;})
       .attr("d", path)
       .style("fill",function(d){   
         var color = "chartreuse"  
@@ -235,50 +173,50 @@ export class NationalMap extends React.Component<void, Props, void> {
         })
         return color;
       })
-      .on("mouseover", mouseover)
-      .on("mouseout", mouseout)
+      .attr("id",function(d){d.shape =this; return "msa"+d.id;})
+      .on("mouseover", this._mouseover)
+      .on("mouseout", this._mouseout)
       .on('click',this._msaClick);
+  }
 
+  _msaClick (d) {
+    console.log(d.id);
+    this.context.router.push('/metro/'+d.id);   
+  }
+
+  _mouseout (d){
+    var scope = this;
+
+    var focus = d3.select(".focus")                             
+    focus.attr("transform", "translate(-100,-100)");
+
+    var oldColor = d3.select(d.shape).style("fill")
+
+    d3.select(d.shape).style("fill",function(d){   
+      var color = "chartreuse"  
+      scope.props[(scope.props.activeComponent+"composite")].forEach(metroArea => {
+        if(metroArea.key == d.id){
+          color = metroArea.color;
+        }
+      })
+      return color;
+    })    
+  }
+
+  _mouseover (d) {
     function shadeRGBColor(color, percent) {
         var f=color.split(","),t=percent<0?0:255,p=percent<0?percent*-1:percent,R=parseInt(f[0].slice(4)),G=parseInt(f[1]),B=parseInt(f[2]);
         return "rgb("+(Math.round((t-R)*p)+R)+","+(Math.round((t-G)*p)+G)+","+(Math.round((t-B)*p)+B)+")";
-    }
+    }                            
 
-    function mouseover(d) {
-      var oldColor = d3.select(d.shape).style("fill")
-      d3.select(d.shape).style("fill",shadeRGBColor(oldColor,-.2))
+    var oldColor = d3.select(d.shape).style("fill")
+    d3.select(d.shape).style("fill",shadeRGBColor(oldColor,-.2))
 
-      var popText = "",
-          name;
+    var popText = d.properties.NAME 
 
-      name = d.properties.NAME;
- 
-      popText += name 
-
-      focus.attr("transform", "translate(50,0)");
-      focus.select("text").text(popText);
-    }
-
-    function mouseout(d) {                              
-      focus.attr("transform", "translate(-100,-100)");
-
-      var oldColor = d3.select(d.shape).style("fill")
-
-      d3.select(d.shape).style("fill",function(d){   
-        var color = "chartreuse"  
-        props[(props.activeComponent+"composite")].forEach(metroArea => {
-          if(metroArea.key == d.id){
-            color = metroArea.color;
-          }
-        })
-        return color;
-      })
-    }
-  }
-
-   _msaClick (d) {
-      console.log(d.id);
-      this.context.router.push('/metro/'+d.id);   
+    var focus = d3.select(".focus") 
+    focus.attr("transform", "translate(50,0)");
+    focus.select("text").text(popText);
   }
 
   _initGraph () {
@@ -286,7 +224,6 @@ export class NationalMap extends React.Component<void, Props, void> {
       return this.props.loadData()
     }
     this._drawGraph(this.props)
-
   }
 
   render () {
