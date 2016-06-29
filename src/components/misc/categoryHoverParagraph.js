c87eaa63f65a590bc6ad0a347abae19e6442983c
RyanDubowsky <r.k.dubowsky@gmail.com>
2016-06-29 18:35:53 -0400

    Added latest paragraph hovertext to paragraph

---
+++ src/components/misc/categoryHoverParagraph.js
@@ -0,0 +1,7 @@
+let categoryHoverParagraph = {
+  density:"measures the relative density of new firms and the percentage of the workforce employed by these young firms. ",
+  diversity:"measures immigration, income mobility and industry diversity. ",
+  fluidity:"measures movement of individuals within and between MSAs. "
+}
+
+export default categoryHoverParagraph
