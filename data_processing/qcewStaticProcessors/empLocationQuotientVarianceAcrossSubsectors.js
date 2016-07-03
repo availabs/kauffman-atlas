2b7feb9145e7e2b2068cdaf35beb8e167b20e20a
paultomchik <paultomchik@gmail.com>
2016-07-02 23:32:03 -0400

    Updated empLocationQuotient... script to include variance and to use 4-digit NAICS.

--- data_processing/qcewStaticProcessors/empLocationQuotientVarianceAcrossSubsectors.js
+++ data_processing/qcewStaticProcessors/empLocationQuotientVarianceAcrossSubsectors.js
@@ -1,4 +1,4 @@
-#!/usr/bin/env node
+#!/usr/bin/env babel-node
 
 'use strict'
 
@@ -15,9 +15,12 @@ const outputFilePath = path.join(__dirname, '../../src/static/data/empLocationQu
 
 const msaIds = Object.keys(JSON.parse(fs.readFileSync('../../src/static/msaIdToName.json')))
 
-const allNaicsCodes = Object.keys(JSON.parse(fs.readFileSync('../../src/static/data/naicsKeys.json'))) 
-const sixDigitNAICS = allNaicsCodes.filter(c => (c.length === 6))
-const fields = ['month1_emplvl', 'month2_emplvl', 'month3_emplvl']
+const allNaicsCodes = Object.keys(JSON.parse(fs.readFileSync('../../src/static/data/naicsKeys.json')))
+const fourDigitNaics = allNaicsCodes.filter(c => (c.split('-')[0].length === 4))
+
+
+const empFields = ['month1_emplvl', 'month2_emplvl', 'month3_emplvl']
+const lqEmpFields = empFields.map(field => `lq_${field}`)
 
 
 const years = _.range(2001, 2017)
@@ -34,12 +37,13 @@ const buildRequestURL = (msa) =>
   `${apiServerAddress}/data/` + 
   `fips${(msa !== '31080') ? `C${msa.slice(0,4)}` : `C3108C3110`}/` +
   `yr${years.join('')}/qtr1234/` + 
-  `ind${sixDigitNAICS.join('')}/` + 
-  `?${fields.map(field => `fields[]=${field}`).join('&')}`
+  `ind${fourDigitNaics.map(c => _.padStart(c, 6, '0')).join('')}/` + 
+  `?${_.union(empFields, lqEmpFields).map(field => `fields[]=${field}`).join('&')}`
 
 
 // This function restructures the key/values style API response into a standard JS object keyed by 'key'.
-const restructureData = d => d.reduce((acc,v)=>(v.key && v.values) ? _.set(acc,v.key,restructureData(v.values)) : v,{})
+const restructureData = d => 
+  d.reduce((acc,v)=>(v.key && v.values) ? _.set(acc,v.key,restructureData(v.values)) : v,{})
 
 // Restructures the response and merges the data for Los Angeles' two distinct MSAs.
 const reformatApiResponse = (data) => 
@@ -53,19 +57,28 @@ const reformatApiResponse = (data) =>
 //returns { <year>: { <quarter>: { <naics>: <avg monthly value of measure> } } }
 const forwardFill = (d) => {
   
-  let lastKnowValues = sixDigitNAICS.reduce((acc, naics) => _.set(acc, naics, 0), {})
+  let lastKnowValues = fourDigitNaics.reduce((acc, naics) => _.set(acc, naics, {emp: 0, lqEmp: 0}), {})
 
   return _.mapValues(d, (byQuarter) =>
             _.mapValues(byQuarter, (byNaics) => 
-              sixDigitNAICS.reduce((acc, naics) => {
+              fourDigitNaics.reduce((acc, naics) => {
 
-                let monthlyLQEmp = fields.map(field => +_.get(byNaics, [naics, field])).filter(Number.isFinite)
+                let monthlyEmp = empFields.map(field => +_.get(byNaics, [naics, field])).filter(Number.isFinite)
 
-                if (monthlyLQEmp.length) {
-                  lastKnowValues[naics] = _.mean(monthlyLQEmp)
+                if (monthlyEmp.length) {
+                  lastKnowValues[naics].emp = _.mean(monthlyEmp)
                 }
 
-                acc[naics] = lastKnowValues[naics]
+                let monthlyLQEmp = lqEmpFields.map(field => +_.get(byNaics, [naics, field])).filter(Number.isFinite)
+
+                if (monthlyLQEmp.length) {
+                  lastKnowValues[naics].lqEmp = _.mean(monthlyLQEmp)
+                }
+
+                acc[naics] = {
+                  emp   : lastKnowValues[naics].emp,
+                  lqEmp : lastKnowValues[naics].lqEmp,
+                }
 
                 return acc
               }, {})
@@ -75,24 +88,42 @@ const forwardFill = (d) => {
 
 
 // Aggregate the byNaics object values into arrays.
-const getMonthlyAveragesByQuarterByYear = (d) => _.mapValues(d, byQtr => _.map(byQtr, byNaics => _.values(byNaics)))
+const getMonthlyAveragesByQuarterByYear = (byYear) => 
+  _.mapValues(byYear, byQtr => 
+    _.mapValues(byQtr, byNaicsForQtr => 
+      _.reduce(byNaicsForQtr, (acc, forNaics) => { 
+        acc.emp.push(forNaics.emp)
+        acc.lqEmp.push(forNaics.lqEmp)
+        return acc
+      }, { emp: [], lqEmp: [] })
+    )
+  )
 
 
 // Returns an object, keyed by year, with values representing 
 // the variance in lq_emplvl across all months and all 6-digit NAICS. 
 const computeDispersionMeasuresByQuarterByYear = (lqEmpAvgsByQtrByYear) => 
         _.mapValues(lqEmpAvgsByQtrByYear, lqEmpAvgsByByQtr =>
-          _.mapValues(lqEmpAvgsByByQtr, avgsForQuarter => {
+          _.mapValues(lqEmpAvgsByByQtr, monthlyAvgsForQtr => {
 
-            let total = _.sum(avgsForQuarter)
-            let shares = avgsForQuarter.map(avg => (avg/total))
+            let topEmp = _.takeRight(monthlyAvgsForQtr.emp.sort(), 100)
+            let topLQEmp = _.takeRight(monthlyAvgsForQtr.lqEmp.sort(), 100)
+
+            let total = _.sum(topEmp)
+            let topEmpShares = topEmp.map(empAvg => (empAvg/total))
+            //let topEmpShares = monthlyAvgsForQtr.emp.map(empAvg => (empAvg/total))
+
+            let lqEmpMean = _.mean(topLQEmp)
+            let lqEmpVariance = _.meanBy(topLQEmp, forNaics => ((forNaics-lqEmpMean)*(forNaics-lqEmpMean)))
 
             return {
-              hhi_2: _(shares).map(share => (share*share)).sum(),
-              hhi_3: _(shares).map(share => (share*share*share)).sum(),
-              hhi_4: _(shares).map(share => (share*share*share*share)).sum(),
+              hhi_2: _(topEmpShares).map(share => (share*share)).sum(),
+              hhi_3: _(topEmpShares).map(share => (share*share*share)).sum(),
+              hhi_4: _(topEmpShares).map(share => (share*share*share*share)).sum(),
 
-              shannon: (-1 * _(shares).filter().map(share => (share * Math.log(share))).sum()),
+              shannon: (-1 * _(topEmpShares).filter().map(share => (share * Math.log(share))).sum()),
+
+              lqEmpVariance,
             }
           })
        )
@@ -104,11 +135,13 @@ const aggregateDispersionMeasuresByYear = (dispMeasuresByQtrByYear) =>
       hhi_3: _(dispMeasuresByQtr).map(dispMeasures => dispMeasures.hhi_3).mean(),
       hhi_4: _(dispMeasuresByQtr).map(dispMeasures => dispMeasures.hhi_4).mean(),
       shannon: _(dispMeasuresByQtr).map(dispMeasures => dispMeasures.shannon).mean(),
+      lqEmpVariance: _(dispMeasuresByQtr).map(dispMeasures => dispMeasures.lqEmpVariance).mean(),
     })
   )
 
+const tapLog = (d) => { console.log(JSON.stringify(d, null, 4)); return d }
 
-const getYearlyEmpVarianceForMSA = (acc, msa, cb) => {
+const getDispersionStatistics = (acc, msa, cb) => {
   
   console.time(msa)
   let stopTimer = (d) => { console.timeEnd(msa); return d }
@@ -116,18 +149,19 @@ const getYearlyEmpVarianceForMSA = (acc, msa, cb) => {
   fetch(buildRequestURL(msa))
     .then(handleFetchErrors)
     .then(response => response.json())
-    .then(stopTimer)
     .then(reformatApiResponse)
     .then(forwardFill)
+    .then(tapLog)
     .then(getMonthlyAveragesByQuarterByYear)
     .then(computeDispersionMeasuresByQuarterByYear)
     .then(aggregateDispersionMeasuresByYear)
+    .then(stopTimer)
     .then((d) => cb(null, _.set(acc, msa, d)))
     .catch(cb)
 }
 
 
-async.reduce(msaIds, {}, getYearlyEmpVarianceForMSA, (err, yearlyEmpVarianceByMSA) => {
+async.reduce(msaIds, {}, getDispersionStatistics, (err, yearlyEmpVarianceByMSA) => {
 
   if (err) { return console.error(err) }
 
